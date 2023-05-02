60a3a4270d77633c52ca20fbc5987f7fdaa97a1b
PoornaSankalana <sanka2016pps@gmail.com>
2023-05-02 14:18:51 +0530

    implementing nav bar

--- Client/src/components/common/header/header.jsx
+++ Client/src/components/common/header/header.jsx
@@ -1,194 +1,90 @@
-/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import styled from "styled-components";
-import { Link as LinkR, Navigate } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
-import { Link as LinkScroll } from "react-scroll";
-// import Onlylogo from "../../Assets/Images/OnlyLogoColored.svg";
-import "./header.css";
-import { UserOutlined } from "@ant-design/icons";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import "./header.css";
-
-export const NavbarContainer = styled.div`
-	display: flex;
-	width: 100%;
-	justify-content: space-between;
-	height: 80px;
-	z-index: 1;
-	padding: 0 24px;
-`;
-
-export const NavLogo = styled(LinkR)`
-	color: #fff;
-	justify-self: self-start;
-	cursor: pointer;
-	font-size: 1.5rem;
-	display: flex;
-	align-items: center;
-	font-weight: bold;
-	text-decoration: none;
-`;
-
-export const MobileIcon = styled.div`
-	display: none;
-
-	@media screen and (max-width: 768px) {
-		display: block;
-		position: absolute;
-		top: 0;
-		right: 0;
-		transform: translate(-100%, 60%);
-		cursor: pointer;
-		font-size: 1.8rem;
-		color: #fff;
-	}
-`;
-
-export const NavMenu = styled.ul`
-	display: flex;
-	align-items: center;
-	list-style: none;
-	text-align: center;
-	margin-right: 22px;
-
-	@media screen and (max-width: 768px) {
-		display: none;
-	}
-`;
-
-export const NavItem = styled.li`
-	height: 80px;
-`;
-
-export const NavLinks = styled(LinkR)`
-	color: #fff;
-	display: flex;
-	align-items: center;
-	padding: 0.1rem;
-	padding-left: 1.5rem;
-	text-decoration: none;
-	height: 100%;
-	cursor: pointer;
-
-	&.active {
-		border-bottom: 3px solid #0c6e7b;
-	}
-
-	&:hover {
-		color: #009a4c;
-		transition: 0.2s ease-in-out;
-	}
-`;
-
-export const NavButton = styled.nav`
-	display: flex;
-	align-items: center;
-
-	@media screen and (max-width: 768px) {
-		display: none;
-	}
-`;
-
-export const NavBtnLink = styled(LinkR)`
-	border-radius: 50px;
-	background: #16a34a;
-	white-space: nowrap;
-	padding: 10px 22px;
-	color: white;
-	font-size: 16px;
-	outline: none;
-	border: none;
-	cursor: pointer;
-	transform: all 0.3s ease-in-out;
-	text-decoration: none;
-
-	&:hover {
-		transition: all 0.3s ease-in-out;
-		background: white;
-		color: #16a34a;
-	}
-`;
 
 const Header = () => {
-	const navigate = useNavigate();
-	const login = localStorage.getItem("loggedIn");
-	console.log(login);
-
-	const logout = () => {
-		localStorage.clear();
-		navigate("/login");
-		console.log("logout");
-		// axios
-		//     .post("/api/logout", {
-		//         headers: {
-		//             authToken: localStorage.getItem("authToken"),
-		//         },
-		//     })
-		//     .then((res) => {
-		//         console.log(res);
-		//         console.log("logout success");
-		//         localStorage.clear();
-		//     });
-	};
-
-	const profileClicked = () => {
-		navigate("/login");
-	};
-
 	return (
-		<>
-			<nav className="nav">
-				<div className="navContainer">
-					<NavLogo to="/">
-						{/* <img src={Onlylogo} className="logo" alt="logo" /> */}
-						<p className="text-green-600">
-							BrainTech Institute
-						</p>
-					</NavLogo>
-					<NavMenu>
-						<NavItem>
-							<NavLinks to="/about"> About </NavLinks>
-						</NavItem>
-						<NavItem>
-							<NavLinks to="/contact"> Contact </NavLinks>
-						</NavItem>
-						<NavItem>
-							<NavLinks to="/services"> Events </NavLinks>
-						</NavItem>
-						{login ? (
-							<NavItem>
-								<NavLinks onClick={logout}>
-									Logout
-								</NavLinks>
-							</NavItem>
-						) : (
-							<NavItem>
-								<NavLinks to="/auth/login">
-									{" "}
-									Sign Up{" "}
-								</NavLinks>
-							</NavItem>
-						)}
-					</NavMenu>
-					{login ? (
-						<img
-							src="https://icon-library.com/images/profile-icon-white/profile-icon-white-7.jpg"
-							className="w-12 cursor-pointer mx-4 my-4 h-12"
-							onClick={profileClicked}
-						/>
-					) : (
-						<NavButton>
-							<NavBtnLink to="/auth/login">
-								{" "}
-								Login{" "}
-							</NavBtnLink>
-						</NavButton>
-					)}
+		// <!-- Navbar -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			{/* <!-- Container wrapper --> */}
+			<div class="container">
+				{/* <!-- Navbar brand --> */}
+				<a
+					class="navbar-brand me-2 mt-2"
+					href="https://mdbgo.com/">
+					<img
+						src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+						height="16"
+						alt="MDB Logo"
+						loading="lazy"
+						style={{ marginTop: "-8px" }}
+					/>
+				</a>
+
+				{/* <!-- Toggle button --> */}
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-mdb-toggle="collapse"
+					data-mdb-target="#navbarButtonsExample"
+					aria-controls="navbarButtonsExample"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<i class="fas fa-bars"></i>
+				</button>
+
+				{/* <!-- Collapsible wrapper --> */}
+				<div
+					class="collapse navbar-collapse"
+					id="navbarButtonsExample">
+					{/* <!-- Left links --> */}
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li
+							class="nav-item"
+							style={{ margin: "0px 20px" }}>
+							<a class="nav-link" href="/">
+								Home
+							</a>
+						</li>
+						<li
+							class="nav-item"
+							style={{ margin: "0px 20px" }}>
+							<a class="nav-link" href="#">
+								Locations
+							</a>
+						</li>
+						<li
+							class="nav-item"
+							style={{ margin: "0px 20px" }}>
+							<a class="nav-link" href="#">
+								About Us
+							</a>
+						</li>
+						<li
+							class="nav-item"
+							style={{ margin: "0px 20px" }}>
+							<a class="nav-link" href="#">
+								Contact Us
+							</a>
+						</li>
+					</ul>
+					{/* <!-- Left links --> */}
+
+					<div class="d-flex align-items-center">
+						<button
+							type="button"
+							class="btn btn-link px-3 me-2">
+							Login
+						</button>
+						<button type="button" class="btn btn-primary me-3">
+							Sign up for free
+						</button>
+					</div>
 				</div>
-			</nav>
-		</>
+				{/* <!-- Collapsible wrapper --> */}
+			</div>
+			{/* <!-- Container wrapper --> */}
+		</nav>
 	);
 };
+
 export default Header;
