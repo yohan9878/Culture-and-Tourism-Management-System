eef88c4e17185234a39a4b4f0cf9b265ddf2ce97
PoornaSankalana <sanka2016pps@gmail.com>
2023-05-03 04:03:31 -0700

    cleaning the project files

---
+++ Client/src/components/Slider/SlideCard.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Sdata from "./Sdata";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SlideCard = () => {
+	const settings = {
+		dots: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		appendDots: (dots) => {
+			return <ul style={{ margin: "0px" }}>{dots}</ul>;
+		},
+	};
+	return (
+		<>
+			<Slider {...settings}>
+				{Sdata.map((value, index) => {
+					return (
+						<div
+							style={{
+								backgroundImage: `url(${value.cover})`,
+							}}>
+							<div
+								className="box d_flex top"
+								key={index}
+								style={{
+									backgroundImage: `url(${value.cover})`,
+								}}>
+								<div className="left">
+									<h1>{value.title}</h1>
+									<p>{value.desc}</p>
+									<button className="btn-primary">
+										Visit Collections
+									</button>
+								</div>
+								<div
+									className="right"
+									style={{
+										backgroundImage: `url(${value.cover})`,
+									}}></div>
+							</div>
+						</div>
+					);
+				})}
+			</Slider>
+		</>
+	);
+};
+
+export default SlideCard;
