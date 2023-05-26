6dc37333944c9e30354a4a2247d42293a2b3ef53
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2023-05-26 19:36:05 +0530

    commenting lines

--- Client/src/components/updateProduct/index.jsx
+++ Client/src/components/updateProduct/index.jsx
@@ -20,7 +20,7 @@ import axios from "axios";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import SliderHome from "../slider/slider";
+// import SliderHome from "../slider/slider";
 import { useParams } from "react-router-dom";
 
 const blue = {
@@ -781,7 +781,7 @@ export default function UpdateProduct() {
 								marginTop: "0px",
 								marginBottom: "-10px",
 							}}>
-							<SliderHome />
+							{/* <SliderHome /> */}
 						</div>
 					</Grid>
 				</Grid>
